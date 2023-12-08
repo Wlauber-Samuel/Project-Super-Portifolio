08bdc3a6491881c5e418ad20276c62d5b7f8a566
Wlauber <wlaubersmuel@gmail.com>
2023-12-07 22:03:35 -0300

    requisito 04 e 05 concluídos

--- projects/views.py
+++ projects/views.py
@@ -1,13 +1,47 @@
 from rest_framework import viewsets
-from .models import Profile, Project
-from .serializers import ProfileSerializer, ProjectSerializer
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from django.shortcuts import render
+from .models import Profile, Project, CertifyingInstitution, Certificate
+from .serializers import (
+    ProfileSerializer,
+    ProjectSerializer,
+    CertifyingInstitutionSerializer,
+    CertificateSerializer
+)
 
 
 class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
 
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [AllowAny()]
+        return [IsAuthenticated()]
+
+    def retrieve(self, request, *args, **kwargs):
+        if request.method == 'GET':
+            profile = Profile.objects.get(id=kwargs['pk'])
+
+            context = {
+                'profile': profile,
+                'projects': profile.projects.all()
+            }
+
+            return render(request, 'profile_detail.html', context)
+        return super().retrieve(request, *args, **kwargs)
+
 
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+
+
+class CertifyingInstitutionViewSet(viewsets.ModelViewSet):
+    queryset = CertifyingInstitution.objects.all()
+    serializer_class = CertifyingInstitutionSerializer
+
+
+class CertificateViewSet(viewsets.ModelViewSet):
+    queryset = Certificate.objects.all()
+    serializer_class = CertificateSerializer
